53d49bd8cbdc6d6c7222f1dd8c6da9cd1d5cb2f3
fuzhengchao <fuzhengchao@fuzhengchaodeMacBook-Pro.local>
2016-03-12 20:03:43 +0800

    添加归并排序算法。

--- ch02/main.c
+++ ch02/main.c
@@ -1,15 +1,36 @@
 #include <stdio.h>
 
+
+/** 打印数组*/
+void printArray(int array[], int start, int end);
+
 /**插入排序声明*/
 void insertSort(int array[], int n);
-/** 打印数组*/
-void printArray(int array[], int n);
+
+/**归并排序*/
+void mergeSort(int array[], int start, int end,  int tmpArray[]);
+/**合并一数组前后两部分， 前后部分均为有序状态*/
+void merge(int array[], int start, int middle, int end, int mergedArray[]);
+
 
 int main () {
-    int array[10] = {5, 6, 3, 9, 2, 8, 6, 4, 10, 12};
-    insertSort(array, 10);
-    printArray(array, 10);
+    int array[10] = {11, 12, 5, 9, 14, 2, 13, 9, 21, 12};
+    // insertSort(array, 10);
+
+    int tmpArray[10];
+    mergeSort(array, 0, 9, tmpArray);
+    printArray(array, 0, 10);
     return 0;
+}
+
+
+/** 打印数组*/
+void printArray(int array[], int start, int end) {
+    for (int i = start; i < end; i++) {
+        printf("%d ",  array[i]);
+    }
+
+    printf("\n");
 }
 
 
@@ -31,13 +52,60 @@ void insertSort(int array[], int n) {
 
         array[insertIndex] = insertNum;
     }
+
+
+    printArray(array, 0, n);
 }
 
-/** 打印数组*/
-void printArray(int array[], int n) {
-    for (int i = 0; i < n; i++) {
-        printf("%d ",  array[i]);
+
+/** 归并排序*/
+void mergeSort(int array[], int start, int end, int tmpArray[]) {
+    if (start < end) {
+        int middle = (start + end) / 2;
+        mergeSort(array, start, middle, tmpArray);
+        mergeSort(array,  middle + 1, end, tmpArray);
+        merge(array, start, middle, end, tmpArray);
+    }
+}
+
+/**合并一数组前后两部分， 前后部分均为有序状态*/
+void merge(int array[], int start, int middle, int end, int mergedArray[]) {
+    int left = start;
+    int right = middle + 1;
+    int index = start;
+
+    while (left <= middle && right <= end) {
+        if (array[left] > array[right]) {       
+            mergedArray[index++] = array[right++];
+        } else {
+            mergedArray[index++] = array[left++];
+        }
     }
 
-    printf("\n");
+    while (left <= middle) {
+        mergedArray[index++] = array[left++];
+    }
+
+    while (right <= end) {
+        mergedArray[index++] = array[right++];
+    }
+
+    for (int i = start; i < index; i++) {
+        array[i] = mergedArray[i];
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
