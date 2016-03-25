d2d5d8282b065bfde77af3e5a46147ec03677bca
fuzhengchao <fuzhengchao@baidudeMacBook-Pro.local>
2016-03-25 17:17:47 +0800

    添加快排部分代码

--- ch07/main.c
+++ ch07/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 
-// 获取数组array中start到end之间的基点并快排调整
+// 获取数组array中start到end之间的基点并快排调整,返回基点index
 int partition(int array[], int start, int end);
 
 // 快速排序
@@ -9,4 +9,10 @@ void quickSort(int array[], int start, int end);
 
 int main() {
 	return 0;
+}
+
+
+int partition(int array[], int start, int end) {
+	int baseIndex, temp;
+	baseIndex = (start + end) / 2;
 }
