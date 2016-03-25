04a5925aca42fd1bde1294b84c1fa33f31e512ea
fuzhengchao <fuzhengchao@baidudeMacBook-Pro.local>
2016-03-25 16:51:58 +0800

    添加快速排序代码

---
+++ ch07/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+// 获取数组array中start到end之间的基点并快排调整
+int partition(int array[], int start, int end);
+
+// 快速排序
+void quickSort(int array[], int start, int end);
+
+int main() {
+	return 0;
+}
